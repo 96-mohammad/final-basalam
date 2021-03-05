4def62c0ea55f1655cd9d59db4e4b0636db95fff
mohammad ahmadi <mh_ahmadi96@yahoo.com>
2021-03-05 17:07:49 +0330

    i have change javascript

--- js/index.js
+++ js/index.js
@@ -2,64 +2,139 @@
 let arrPlus = document.querySelectorAll('.plus-sign');
 let arrMinus = document.querySelectorAll('.minus-sign');
 let boxNumber = document.querySelectorAll('.body__count__calc-num');
+let navNumber = document.querySelector('.header__nav-not-number');
 
+let arr = Array.prototype.slice.call(arrPlus);
+let array = Array.prototype.slice.call(arrMinus);
+let list = Array.prototype.slice.call(boxNumber);
 
-function changeDom1() {
-    let sum = 1;   
+function changeDom(list, nav) {
+    let sum, sum1, sum2, sum3;
+    
+    sum1 = 1;
+    sum2 = 1;
+    sum3 = 1;
+    sum = sum1 + sum2 + sum3;
 
-    boxNumber[0].textContent = sum;
+    list[0].textContent = sum1;
+    list[1].textContent = sum2;
+    list[2].textContent = sum3;
+    nav.textContent = sum;
+
+    arr.forEach((cur,index) => {
+        if (index === 0) {
+            cur.addEventListener('click', () => {
+            sum1++;
+            list[0].textContent = sum1;
+            sum = sum1 + sum2 + sum3;
+            nav.textContent = sum;
+            });        
+        }
+        else if (index === 1) {
+            cur.addEventListener('click', () => {
+                sum2++;
+                list[1].textContent = sum2;
+                sum = sum1 + sum2 + sum3;
+                nav.textContent = sum;
+            });
+        }
+        else cur.addEventListener('click', () => {
+                sum3++;
+                list[2].textContent = sum3;
+                sum = sum1 + sum2 + sum3;
+                nav.textContent = sum;
+        });
+    });
+
+    array.forEach((cur,index) => {
+        if (index === 0) {
+            cur.addEventListener('click', () => {
+            sum1--;
+            if (sum1 >= 1) {                
+                list[0].textContent = sum1; 
+                sum = sum1 + sum2 + sum3;
+                nav.textContent = sum;                
+            } else sum1 = 1;
+            });        
+        }
+        else if (index === 1) {
+            cur.addEventListener('click', () => {
+                sum2--;
+                if (sum2 >= 1) {                
+                    list[1].textContent = sum2; 
+                    sum = sum1 + sum2 + sum3;
+                    nav.textContent = sum;               
+                } else sum2 = 1;
+            });
+        }
+        else cur.addEventListener('click', () => {
+            sum3--;
+            if (sum3 >= 1) {                
+                list[2].textContent = sum3; 
+                sum = sum1 + sum2 + sum3;
+                nav.textContent = sum;               
+            } else sum3 = 1;
+        });
+    });
+};
+
+changeDom(boxNumber, navNumber);
+
+/*
+function renderDom(arrPlus,arrMinus,list,navNumber) {    
+    let sum1 = 1;
+    let sum2 = 1;
+    let sum3 = 1;
+    let count = sum1 + sum2 + sum3;
+
+    list[0].textContent = sum1;
+    list[1].textContent = sum2;
+    list[2].textContent = sum3;
+    navNumber.textContent = count;
 
     arrPlus[0].addEventListener('click', () => {
-        sum++;
-        boxNumber[0].textContent = sum;
-    }); 
+        sum1++;
+        list[0].textContent = sum1;
+        count = sum1 + sum2 + sum3;        
+        navNumber.textContent = count;
+    });
+    arrPlus[1].addEventListener('click', () => {
+        sum2++;
+        list[1].textContent = sum2;
+        count = sum1 + sum2 + sum3;
+        navNumber.textContent = count;
+    });
+    arrPlus[2].addEventListener('click', () => {
+        sum3++;
+        list[2].textContent = sum3;
+        count = sum1 + sum2 + sum3;
+        navNumber.textContent = count;    
+    });  
     
     arrMinus[0].addEventListener('click', () => {
-        sum--;
-        if (sum >= 1) {
-        boxNumber[0].textContent = sum;
-        }
+        sum1--;
+        if (sum1 >= 1) {
+        list[0].textContent = sum1;
+        count = sum1 + sum2 + sum3;
+        navNumber.textContent = count;
+        } else sum1 = 1;
     });
-};
-
-function changeDom2() {
-    let sum = 1;    
-
-    boxNumber[1].textContent = sum;
-
-    arrPlus[1].addEventListener('click', () => {
-        sum++;
-        boxNumber[1].textContent = sum;
-    });
-    
     arrMinus[1].addEventListener('click', () => {
-        sum--;
-        if (sum >= 1) {
-        boxNumber[1].textContent = sum;
-        }
+        sum2--;
+        if (sum2 >= 1) {
+        list[1].textContent = sum2;
+        count = sum1 + sum2 + sum3;
+        navNumber.textContent = count;
+        } else sum2 = 1;
     });
-};
-
-function changeDom3() {
-    let sum = 1;    
-
-    boxNumber[2].textContent = sum;
-
-    arrPlus[2].addEventListener('click', () => {
-        sum++;
-        boxNumber[2].textContent = sum;
-    });
-    
     arrMinus[2].addEventListener('click', () => {
-        sum--;
-        if (sum >= 1) {
-        boxNumber[2].textContent = sum;
-        }
-    });
+        sum3--;
+        if (sum3 >= 1) {
+        list[2].textContent = sum3;
+        count = sum1 + sum2 + sum3;
+        navNumber.textContent = count;
+        } else sum3 = 1;
+    }); 
+    renderDom(arrPlus,arrMinus,boxNumber,navNumber);     
 };
-
-changeDom1();
-changeDom2();
-changeDom3();
-
-
+*/
